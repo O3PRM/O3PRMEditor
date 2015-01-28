b974a3d967fea63feb62436d59387243e18268c0
Lionel Torti <lionel.torti@gmail.com>
2015-01-28 12:34:48 +0100

    EditorController almost finished.

--- src/controllers/editorController.cpp
+++ src/controllers/editorController.cpp
@@ -1,6 +1,7 @@
 #include "controllers/editorController.h"
 
 #include <QMessageBox>
+#include <QFileDialog>
 
 #include "uis/mainwindow.h"
 #include "controllers/projectcontroller.h"
@@ -36,50 +37,120 @@ namespace o3prm
         }
     }
 
-    bool EditorController::__openFile(const QFileInfo& fileinfo)
+    void EditorController::saveFile()
     {
-        // If already opend, set focus on it
-        if (__openFiles.contains(fileinfo.absoluteFilePath()))
+        // If argument is 0, get the current document
+        auto sci = __currentDocument(); 
+
+        // If there is any editor open
+        if (sci)
         {
-            auto sci = __openFiles.value(fileinfo.absoluteFilePath());
-            __mainWidget->mainWindow()->tabWidget->setCurrentWidget(sci);
-            return true;
-        }
-        // File not open, lets try to open it
-        else
-        {
-            if (not fileinfo.isFile())
+            QString filename = sci->filename();
+            // If file never has been save, go to save as
+            if ( filename.isEmpty() )
             {
-                return false;
+                saveAsFile(sci);
+            }
+            if (not __saveFile(filename, sci))
+            {
+                auto title = tr("Error saving file");
+                auto msg = tr("Could no save %1.").arg(filename) ;
+                QMessageBox::warning(__mainWidget, title, msg);
+            }
+        }
+    }
+
+    void EditorController::saveAsFile(QsciScintillaExtended *sci, QString dir)
+    {
+        // If argument is 0, get the current editor
+        if ( sci == 0 )
+        {
+            sci = __currentDocument();
+        }
+
+        if (sci)
+        {
+            auto path = __findWhereToSave(sci, dir);
+            auto filename = __askForFileName(sci, path);
+
+            if (not filename.isEmpty())
+            {
+                sci->setFilename(filename);
+
+                if (not __saveFile(filename, sci))
+                {
+                    auto title = tr("Error saving file");
+                    auto msg = tr("Could no save %1.").arg(filename) ;
+                    QMessageBox::warning(__mainWidget, title, msg);
+                }
+            }
+        }
+    }
+
+    void EditorController::closeFile(int index)
+    {
+        auto sci = __findDocument(index); // updates index
+
+        // if there is an open file then of file has changed, ask for save
+        if (sci and __askForSaveIfChanged(sci, index))
+        {
+            __mainWidget->mainwindow()->tabWidget->removeTab( index );
+
+            if ( __mainWidget->mainwindow()->tabWidget->count() == 0 ) 
+            {
+                __toggleEditorMenus(false);
+            }
+            else if ( index == 0 )
+            {
+                auto new_index = (index?index-1:index); // previous tab or last one left
+                __mainWidget->mainwindow()->tabWidget->widget( new_index )->setFocus();
             }
 
-            // We open the file
-            QFile file( fileinfo.absoluteFilePath() );
-
-            if (file.open( QIODevice::ReadOnly | QIODevice::Text ) )
-            {
-                // We charge the file,
-                auto text = QString::fromUtf8(file.readAll());
-                file.close();
-
-                // We create the QScintilla view of the file
-                auto lexer = QsciScintillaExtended::None;
-                auto sci =  new QsciScintillaExtended( lexer, __mainWidget->mainWindow()->tabWidget );
-                __initialize(sci, fileinfo.fileName(), fileinfo.absoluteFilePath(), text);
-
-                // Add the file to the view
-                int index = __mainWidget->mainWindow()->tabWidget->addTab( sci, fileinfo.fileName() );
-                __mainWidget->mainWindow()->tabWidget->setCurrentWidget( sci );
-                sci->setFocus();
-
-                // Update members and view
-                __openFiles.insert( fileinfo.absoluteFilePath(), sci );
-                __toggleEditorMenus(true);
-
-                return true;
-            }
-            return false;
+            QFileInfo info(sci->filename()); // TODO: this may not be necessary
+            emit fileClosed(info.absolutePath());
+            sci->deleteLater();
         }
+    }
+
+    void EditorController::saveAllFiles()
+    {
+        bool success = true;
+        for ( int i = 0; i < __mainWidget->mainWindow()->tabWidget->count(); i++ ) 
+        {
+            auto sci = __findDocument(i);
+            success = success and __saveFile(sci->filename(), sci);
+        }
+
+        if (not success)
+        {
+            auto title = tr("Error saving files");
+            auto msg = tr("Could not save all files.");
+            QMessageBox::warning(__mainWidget, title, msg);
+        }
+    }
+
+    void EditorController::closeAllFiles()
+    {
+        for ( int i = __mainWidget->mainWindow()->tabWidget->count() - 1; i >= 0 ; i-- ) 
+        {
+            closeFile(i);
+        }
+    }
+
+    QsciScintillaExtended* EditorController::__currentDocument() const
+    {
+        return qobject_cast<QsciScintillaExtended *>( __mainWidget->mainwindow()->tabWidget->currentWidget() );
+    }
+
+    QsciScintillaExtended* EditorController::__findDocument(int& index)
+    {
+        if (index >= 0 and index < __mainWidget->mainWindow()->tabWidget->count())
+        {
+            index = __mainWidget->mainWindow()->tabWidget->currentIndex();
+            return __currentDocument();
+        }
+        auto obj = __mainWidget->mainWindow()->tabWidget->widget(index);
+        return qobject_cast<QsciScintillaExtended*>(obj);
     }
 
     void EditorController::__toggleEditorMenus(bool show)
@@ -137,23 +208,150 @@ namespace o3prm
         connect( sci, SIGNAL( filenameChanged( QString,QString ) ), this, SLOT( onDocumentRenamed( QString,QString ) ) );
     }
 
-    void EditorController::saveFile(QString path)
+    bool EditorController::__openFile(const QFileInfo& fileinfo)
     {
+        // If already opend, set focus on it
+        if (__openFiles.contains(fileinfo.absoluteFilePath()))
+        {
+            auto sci = __openFiles.value(fileinfo.absoluteFilePath());
+            __mainWidget->mainWindow()->tabWidget->setCurrentWidget(sci);
+            return true;
+        }
+        // File not open, lets try to open it
+        else
+        {
+            if (not fileinfo.isFile())
+            {
+                return false;
+            }
 
+            // We open the file
+            QFile file( fileinfo.absoluteFilePath() );
+
+            if (file.open( QIODevice::ReadOnly | QIODevice::Text ) )
+            {
+                // We charge the file,
+                auto text = QString::fromUtf8(file.readAll());
+                file.close();
+
+                // We create the QScintilla view of the file
+                auto lexer = QsciScintillaExtended::None;
+                auto sci =  new QsciScintillaExtended( lexer, __mainWidget->mainWindow()->tabWidget );
+                __initialize(sci, fileinfo.fileName(), fileinfo.absoluteFilePath(), text);
+
+                // Add the file to the view
+                int index = __mainWidget->mainWindow()->tabWidget->addTab( sci, fileinfo.fileName() );
+                __mainWidget->mainWindow()->tabWidget->setCurrentWidget( sci );
+                sci->setFocus();
+
+                // Update members and view
+                __openFiles.insert( fileinfo.absoluteFilePath(), sci );
+                __toggleEditorMenus(true);
+
+                emit fileOpened(fileinfo.absolutePath(), sci);
+                return true;
+            }
+            return false;
+        }
     }
 
-    void EditorController::closeFile(QString path)
+    /// TODO: first arg should be removed
+    bool EditorController::__saveFile(const QString& filename, QsciScintillaExtended * sci)
     {
+        QFile file( filename );
 
+        if ( file.open( QIODevice::WriteOnly | QIODevice::Text ) )
+        {
+            file.write( sci->text().toUtf8() );
+            file.close();
+            sci->setModified( false );
+
+            QFileInfo info(file);
+            emit fileSaved(info.absolutePath(), sci);
+
+            return true;
+        }
+        return false;
     }
 
-    void EditorController::saveAllFiles()
+    QString EditorController::__findWhereToSave(QsciScintillaExtended * sci, const QString& dir)
     {
-
+        if ( dir.isEmpty() and not sci->filename().isEmpty() )
+        {
+            return sci->filename();
+        }
+        else if ( dir.isEmpty() and __mainWidget->projectController()->currentProject() )
+        {
+            return __mainWidget->projectController()->currentProject()->dir().absolutePath();
+        }
+        else  // if ( dir.isEmpty() ) or everything else failed
+        {
+            return QDir::homePath();
+        }
     }
 
-    void EditorController::closeAllFiles()
+    QString EditorController::__askForFileName(QsciScintillaExtended * sci, const QString& dir)
     {
+        const QString o3prmlFilter = tr( "Class or system files (*.o3prml)" );
+        const QString o3prmrFilter = tr( "Request files (*.o3prmr)" );
+
+        QString filters, selectedFilter;
+
+        // Set pre-selectionned filter
+        if ( sci->lexerEnum() == QsciScintillaExtended::O3prmr )
+        {
+            filters = o3prmrFilter + ";;" + o3prmlFilter;
+        }
+        else
+        {
+            filters = o3prmlFilter + ";;" + o3prmrFilter;
+        }
+
+        auto title = tr( "Save file as..." );
+        auto filename = QFileDialog::getSaveFileName(__mainWidget, title, dir, filters, &selectedFilter);
+
+        if (selectedFilter == o3prmlFilter and not filename.endsWith(".o3prml"))
+        {
+            filename += ".o3prml";
+        }
+        else if (selectedFilter == o3prmrFilter and not filename.endsWith(".o3prmr"))
+        {
+            filename += ".o3prmr";
+        }
+
+        return filename;
+    }
+
+    bool EditorController::__askForSaveIfChanged(QsciScintillaExtended* sci, int index)
+    {
+        if ( sci->isModified() )
+        {
+            auto title = tr( "Save file ?" );
+            auto filename = __mainWidget->mainWindow()->tabWidget->tabText(index);
+            auto msg = tr( "%1 has unsaved modifications, do you want to save them ?" ).arg(filename);
+            auto buttons = QMessageBox::Ok | QMessageBox::Discard | QMessageBox::Cancel;
+
+            switch ( QMessageBox::question( __mainWidget, title, msg, buttons, QMessageBox::Ok ))
+            {
+                case QMessageBox::Ok :
+                    {
+                        // If we can't save the file, we don't close it.
+                        if ( __saveFile(sci->filename(), sci) )
+                        {
+                            return true;
+                        }
+                    }
+                case QMessageBox::Discard :
+                    {
+                        return true;
+                    }
+                default:
+                    {
+                        return false;
+                    }
+            }
+        }
+        return true;
     }
 
 } // o3prm
--- src/controllers/editorController.h
+++ src/controllers/editorController.h
@@ -27,20 +27,44 @@ namespace o3prm
             void setupConnections();
 
         public slots:
+            /// Open file at path in a new tab
             void openFile(QString path);
 
-            void saveFile(QString path);
+            /// Save the current document
+            void saveFile();
 
-            void closeFile(QString path);
+            /// Copy the file somewhere
+            void saveAsFile(QsciScintillaExtended *sci=0, QString dir="");
 
+            /// Save all files
             void saveAllFiles();
 
+            /// Close file at index or curent file if index == -1
+            void closeFile(int index=-1);
+
+            /// Close all files
             void closeAllFiles();
+
+        signals:
+            /// Emited when a file is opened
+            void fileOpened(QString path, QsciScintillaExtended *sci);
+
+            /// Emited when a file is saved 
+            void fileSaved(QString path, QsciScintillaExtended *sci);
+
+            /// Emited when a file is closed
+            void fileClosed(QString path);
 
         private:
             MainWindow *__mainWidget;
 
             QHash<QString, QsciScintillaExtended *> __openFiles;
+
+            /// Return the current document, the open tab, or 0 if any.
+            QsciScintillaExtended* __currentDocument() const;
+
+            /// Returns the document and update index with the proper value.
+            QsciScintillaExtended* __findDocument(int& index);
 
             bool __openFile(const QFileInfo& fileinfo);
 
@@ -50,6 +74,18 @@ namespace o3prm
                              const QString& title,
                              const QString& path,
                              const QString& text);
+
+            /// Save the file, even if it is unmodified.
+            bool __saveFile(const QString& filename, QsciScintillaExtended * sci);
+
+            /// Return a path where the file should be saved.
+            QString __findWhereToSave(QsciScintillaExtended * sci, const QString& dir);
+
+            /// Shows a dialog starting at path that ask for a file name.
+            QString __askForFileName(QsciScintillaExtended * sci, const QString& path);
+
+            /// Returns true if the file was saved or if the user did not want to save it.
+            bool __askForSaveIfChanged(QsciScintillaExtended* sci, int index);
     };
 }
 
